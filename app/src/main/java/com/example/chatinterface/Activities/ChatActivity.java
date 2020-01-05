a8e3f820e6e478aa79b7d76a94b6016a3785c484
Tanvigupta96 <guptatannu.34gmail.com>
2020-01-05 16:16:06 +0530

    private chat functionality added

--- app/src/main/java/com/example/chatinterface/Activities/ChatActivity.java
+++ app/src/main/java/com/example/chatinterface/Activities/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.chatinterface.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,22 +13,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.chatinterface.Adapters.MessageAdapter;
 import com.example.chatinterface.R;
+import com.example.chatinterface.model.Messages;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -42,7 +52,14 @@ public class ChatActivity extends AppCompatActivity {
     private ImageButton sendMessageButton;
     private EditText messageInputText;
     private FirebaseAuth mAuth;
+
     private DatabaseReference RootRef;
+
+    private final List<Messages> messagesList = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private MessageAdapter adapter;
+    private RecyclerView UserMessagesList;
+
 
 
     @Override
@@ -95,7 +112,54 @@ public class ChatActivity extends AppCompatActivity {
         sendMessageButton = findViewById(R.id.send_message_btn);
         messageInputText = findViewById(R.id.input_message);
 
+        adapter = new MessageAdapter(messagesList);
 
+        UserMessagesList = findViewById(R.id.private_messages_list_of_users);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+
+        UserMessagesList.setLayoutManager(linearLayoutManager);
+
+        UserMessagesList.setAdapter(adapter);
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        RootRef.child("Message").child(messageSenderId).child(msgReceiverId).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                Messages messages = dataSnapshot.getValue(Messages.class);
+                messagesList.add(messages);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void SendMesage() {
---
+++ app/src/main/java/com/example/chatinterface/Adapters/MessageAdapter.java
@@ -0,0 +1,156 @@
+package com.example.chatinterface.Adapters;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatinterface.R;
+import com.example.chatinterface.model.Messages;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.graphics.Color.BLACK;
+
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private List<Messages> userMessageList;
+    private FirebaseAuth mAuth;
+    private DatabaseReference usersRef;
+
+
+    public MessageAdapter(List<Messages> userMessageList) {
+        this.userMessageList = userMessageList;
+
+    }
+
+
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView senderMessageText, receiverMessageText;
+        public CircleImageView receiverProfileImage;
+
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            senderMessageText = itemView.findViewById(R.id.sender_msg_text);
+            receiverMessageText = itemView.findViewById(R.id.receiver_msg_text);
+            receiverProfileImage = itemView.findViewById(R.id.message_profile_image);
+
+
+        }
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_msgs_layout, viewGroup, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        return new MessageViewHolder(view);
+
+
+    }
+
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull final MessageViewHolder holder, int i) {
+         String messageSenderId = mAuth.getCurrentUser().getUid();
+         Messages messages = userMessageList.get(i);
+
+
+         String fromUserId = messages.getFrom();
+         String frommessageType = messages.getType();
+
+
+         usersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(fromUserId);
+
+        usersRef.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.hasChild("image"))
+                {
+
+                    String receiverImage = dataSnapshot.child("image").getValue().toString();
+
+                    Picasso.get().load(receiverImage).placeholder(R.drawable.profile_image).into(holder.receiverProfileImage);
+
+                }
+
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });
+
+
+
+        if (frommessageType.equals("text"))
+        {
+            holder.receiverMessageText.setVisibility(View.INVISIBLE);
+            holder.receiverProfileImage.setVisibility(View.INVISIBLE);
+
+            if (fromUserId.equals(messageSenderId))
+            {
+                holder.senderMessageText.setBackgroundResource(R.drawable.sender_msgs_layout);
+                holder.senderMessageText.setTextColor(BLACK);
+                holder.senderMessageText.setText(messages.getMessage());
+
+
+
+            }
+            else {
+                holder.senderMessageText.setVisibility(View.INVISIBLE);
+
+
+                holder.receiverMessageText.setVisibility(View.VISIBLE);
+                holder.receiverProfileImage.setVisibility(View.VISIBLE);
+
+                holder.receiverMessageText.setBackgroundResource(R.drawable.receiver_msgs_layout);
+                holder.receiverMessageText.setTextColor(BLACK);
+                holder.receiverMessageText.setText(messages.getMessage());
+
+            }
+        }
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return userMessageList.size();
+    }
+
+
+}
